23cd8d2daaf74c7947a5fa0ace824ee2b794fdcc
MenaEldeeb <menaeldeeb98@gmail.com>
2026-08-23 00:22:23 +0300

    Update database connection

--- obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c4e62b83552f8f27eb3444b635231d8b801814")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f442b78f0867dd30ded092d42213eb05bc8962f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
