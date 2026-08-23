37f7043808e0cc5022f77bf76593b4a7981c4b06
MenaEldeeb <menaeldeeb98@gmail.com>
2026-08-23 10:58:41 +0300

    Remove exposed connection string

--- obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f442b78f0867dd30ded092d42213eb05bc8962f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23cd8d2daaf74c7947a5fa0ace824ee2b794fdcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
